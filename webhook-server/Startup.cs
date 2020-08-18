47d700e288f2e683a393705eb0d4da7ff36df93e
Tyler Long <tyler4long@gmail.com>
2020-08-18 11:09:50 -0700

    Upgrade dotnet core and dependencies

--- webhook-server/Startup.cs
+++ webhook-server/Startup.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Primitives;
 
 namespace webhook_server
@@ -14,21 +16,18 @@ namespace webhook_server
         {
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
-            
-            app.Run( async (context) =>
+            app.Run(  async (context) =>
             {
                 context.Request.Headers.TryGetValue("Validation-Token", out StringValues validationToken);
                 context.Response.Headers.Add("Validation-Token", validationToken);
                 if (context.Request.Path == "/webhook" && context.Request.Method == "POST")
                 {
-                    using (StreamReader reader = new StreamReader(context.Request.Body, Encoding.UTF8))
-                    {
-                        var str = reader.ReadToEnd();
-                        Console.WriteLine(str);
-                    }
+                    using StreamReader reader = new StreamReader(context.Request.Body, Encoding.UTF8);
+                    var str = await reader.ReadToEndAsync();
+                    Console.WriteLine(str);
                 }
             });
         }
